b1883bec151ddf17077b4d4eb1442070fb5d4452
cgansen <cgansen@gmail.com>
2013-02-14 21:15:03 -0600

    dummy app to echo params
    trying to get visibility into what wufoo is sending

--- app.rb
+++ app.rb
@@ -4,6 +4,13 @@ class CutgroupSignups < Sinatra::Base
   end
   
   post "/signup" do
+    # TODO: sniff access token from params to ensure it's legit
     
+    # s3 = AWS::S3.new
+    # bucket = s3.buckets("cutgroup.smartchicagoapps.org")    # FIXME: set as env var
+    # signup_file = bucket.objects['assets/js/signups.json']  # FIXME: set as env var
+    
+    params.inspect
+
   end  
 end
